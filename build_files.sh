295f45fcf861b27f5eda9dee0198b8759458b97a
Cesar Alejandro <154447276+AGUIRREMTZ@users.noreply.github.com>
2025-11-19 10:48:23 -0600

    Update Python command for collecting static files

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
